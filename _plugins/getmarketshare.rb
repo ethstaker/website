837ca93686d646e271cc4493caa450ad6ebd79fe
hanniabu <hanniabu@gmail.com>
2026-04-30 10:48:55 -0400

    staking survery cta

--- _plugins/getmarketshare.rb
+++ _plugins/getmarketshare.rb
@@ -15,24 +15,25 @@ module Jekyll
   module GetMarketshare
 
     def getmarketshare(rounding_depth=0)
-      puts Rainbow("Fetching data: getmarketshare()").green
+      22
+      # puts Rainbow("Fetching data: getmarketshare()").green
 
-      uri = URI('https://api.dune.com/api/v1/query/1933075/results')
-      uri.query = URI.encode_www_form(limit: 1000)
+      # uri = URI('https://api.dune.com/api/v1/query/1933075/results')
+      # uri.query = URI.encode_www_form(limit: 1000)
 
-      res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-        http.get(uri, { 'X-Dune-API-Key' => ENV['DUNE_KEY'] })
-      end
+      # res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      #   http.get(uri, { 'X-Dune-API-Key' => ENV['DUNE_KEY'] })
+      # end
 
-      unless res.is_a?(Net::HTTPSuccess)
-        abort("ERROR: Fetch failed: getmarketshare()")
-      end
+      # unless res.is_a?(Net::HTTPSuccess)
+      #   abort("ERROR: Fetch failed: getmarketshare()")
+      # end
 
-      data = JSON.parse(res.body)
-      lido_percentage = data.dig("result", "rows", 0, "lido_percentage").round(rounding_depth.to_i)
-      puts lido_percentage
+      # data = JSON.parse(res.body)
+      # lido_percentage = data.dig("result", "rows", 0, "lido_percentage").round(rounding_depth.to_i)
+      # puts lido_percentage
 
-      lido_percentage
+      # lido_percentage
     end
 
   end
